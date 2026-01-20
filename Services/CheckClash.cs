6a35ee2a2234e74fc52ca175aa5e993dc2ebc57f
Manoj Nagarajan <MNAGN@ramboll.com>
2026-01-20 12:44:08 +0530

    clash check

---
+++ Services/CheckClash.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.DB;
+using MyTools.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    public class CheckClash
+    {
+        public static List<Element> getClashingElements(Solid filledRegionVolume, List<Element> elementsInView)
+        {
+            List<Element> clashingList = new List<Element>();
+
+            foreach (Element el in elementsInView)
+            {
+                // Get the accurate Geometry Center
+                XYZ geoCenter = GeometryCenterService.GetTrueGeometryCenter(el);
+
+                // Check if the point is inside your solid volume
+                if (geoCenter != null && filledRegionVolume.Contains(geoCenter))
+                {
+                    clashingList.Add(el);
+                }
+            }
+            return clashingList;
+
+        }
+
+    }
+}
